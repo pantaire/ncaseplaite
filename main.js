6d7697502eae1d94f2dd104c336d19a54b8f19ed
kadda <kadda@taire.de>
2021-03-29 22:54:33 +0200

    added sidenav and responsivity

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+function myFunction(x) {
+    sidenav = document.getElementById("sidenav");
+    if (sidenav.style.display === "block") {
+        sidenav.style.display = "none";
+    }
+    else {
+        sidenav.style.display = "block";
+    }
+    x.classList.toggle("change");
+    console.log("toggled");
+}
